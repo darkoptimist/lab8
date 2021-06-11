430aa6b01417b6b151dc580b1b63487404397395
darkoptimist <darkoptimist1999@gmail.com>
2021-06-11 10:59:40 +0300

    making corrections

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let skills = [
+const skills = [
 	{skillName: "HTML", level: 60, cssClass: "skill-html"},
 	{skillName: "JS", level: 30, cssClass: "skill-js"},
 	{skillName: "C++", level: 70, cssClass: "skill-cpp"},
@@ -7,17 +7,17 @@ let skills = [
 	{skillName: "SQL", level: 50, cssClass: "skill-sql"}
 ];
 
-skillBar = document.querySelector('.skills-list');
+const skillBar = document.querySelector('.skills-list');
 
-skills.forEach(function(item, i, skills){
-	let dtElem = document.createElement('dt');
-	dtElem.setAttribute('class', item.cssClass);
+skills.forEach((item) => {
+	const dtElem = document.createElement('dt');
+	dtElem.classList.add(item.cssClass);
 	dtElem.textContent = item.skillName;
 	skillBar.append(dtElem);
-	ddElem = document.createElement('dd');
-	ddElem.setAttribute('class', 'level');
-	let divElem = document.createElement('div');
-    divElem.setAttribute('style', `width: ${item.level}%;`);
+	const ddElem = document.createElement('dd');
+	ddElem.classList.add('level');
+	const divElem = document.createElement('div');
+    divElem.style.cssText = `width: ${item.level}%;`;
     divElem.textContent = item.level;
 	ddElem.appendChild(divElem);
 	skillBar.append(ddElem);
